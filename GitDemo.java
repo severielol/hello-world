eab1175f0d9fbd069e5c1de1a5151622bf4087ee
thiya81 <79820876+thiya81@users.noreply.github.com>
2021-02-28 21:59:42 -0700

    add name and edited addNumber()
    adding name to Line 2(accidentally did this locally before uploading...whoops)

--- GitDemo.java
+++ GitDemo.java
@@ -4,6 +4,7 @@
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum;
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,7 +15,8 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        previousNumber = previousNumber + num;
+        return previousNumber;
     }
     
     /*****
@@ -45,7 +47,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber;
         return newNumber;
     }
     
